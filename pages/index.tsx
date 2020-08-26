c6322039e7fc911251b2e89db5b1526b3bd17dc0
Kenny Mark <geniounico@outlook.com>
2020-08-26 01:20:49 +0100

    refactoring

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
-import React from "react"
-import SEO from "../src/components/seo"
-import { Box } from '@chakra-ui/core'
-import Contact from '../src/components/sections/contact'
-import Main from '../src/components/sections/main'
-import Portfolio from '../src/components/sections/portfolio'
-import { Fragment } from 'react'
-import Skills from "../src/components/sections/skills"
+import React, { Fragment } from "react"
+import Contact from 'src/components/sections/contact'
+import Main from 'components/sections/main'
+import Portfolio from 'components/sections/portfolio'
+import Skills from "components/sections/skills"
+import SEO from 'components/seo'
 
 
 
@@ -13,19 +11,15 @@ const IndexPage = () => (
 
   <Fragment>
     <SEO title="Home" />
+
     <Main />
 
-    <Box w={['', '', 700,]} mx='auto' mt={20} >
-      <Portfolio />
-    </Box>
+    <Portfolio />
 
-    <Box w={['', '', 700, 1400,]} mx='auto' mt={20} >
-      <Skills />
-    </Box>
+    <Skills />
 
-    <Box w={['', '', '', 700]} mx='auto' mt={20} p={4} >
-      <Contact />
-    </Box>
+    <Contact />
+
   </Fragment>
 
 )
