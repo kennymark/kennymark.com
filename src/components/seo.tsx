1ec9763c1ad2f7ca79358050043f9a2e1491c65d
Kenny Mark <geniounico@outlook.com>
2020-08-26 17:28:02 +0100

    fix description

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -20,7 +20,7 @@ function SEO({ description, lang, title }) {
       meta={[
         {
           name: `description`,
-          content: description ?? meta.description,
+          content: description || meta.description,
         },
         {
           property: `og:title`,
