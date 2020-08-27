45578f24a6b8938d254ce03507ff9ae4d79a081c
Kenny Mark <geniounico@outlook.com>
2020-08-27 03:55:32 +0100

    should fix error now

--- src/components/active-link.tsx
+++ src/components/active-link.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { useColorModeValue } from '@chakra-ui/core'
 
 const ActiveLink = ({ href, children }) => {
   const router = useRouter()
-  const color = useColorModeValue('black', '#38a169')
   let className = children.props.className || ''
 
   if (router.pathname === href) {
