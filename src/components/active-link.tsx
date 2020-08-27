77c6e19dd7c18f5c4faba627d0aacf0c0fa22dbf
Kenny Mark <geniounico@outlook.com>
2020-08-27 02:41:50 +0100

    added active link

--- src/components/active-link.tsx
+++ src/components/active-link.tsx
@@ -7,12 +7,12 @@ const ActiveLink = ({ href, children }) => {
   const router = useRouter()
   const color = useColorModeValue('black', '#38a169')
   let className = children.props.className || ''
-  let style = children.props.style = { color }
+
   if (router.pathname === href) {
     className = `${className} selected`
   }
 
-  return <Link href={href}>{React.cloneElement(children, { className, style })}</Link>
+  return <Link href={href}>{React.cloneElement(children, { className })}</Link>
 }
 
 
