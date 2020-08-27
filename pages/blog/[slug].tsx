3e06b041fa012ddfd8d663c4b82ce06b678148dc
Kenny Mark <geniounico@outlook.com>
2020-08-27 04:02:46 +0100

    fix test

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -4,13 +4,11 @@ import { components } from 'components/mdx/provider'
 import SEO from 'components/seo'
 import fs from 'fs'
 import matter from 'gray-matter'
-import { startCase, } from 'lodash'
 import hydrate from 'next-mdx-remote/hydrate'
 import renderToString from 'next-mdx-remote/render-to-string'
 import path from 'path'
 import React from 'react'
 import Img from "react-cool-img";
-import slugify from 'slug'
 import AuthorCard from 'components/blog/AuthorCard'
 
 function Post({ post }) {
@@ -21,7 +19,7 @@ function Post({ post }) {
   return (
     <Container mb={20} my={10} maxW="md">
 
-      <SEO title={startCase(title)} description={description} />
+      <SEO title={title} description={description} />
 
       <Heading mb={3} fontSize={40} textAlign='center'>{title}</Heading>
 
@@ -46,7 +44,7 @@ function Post({ post }) {
 export const getStaticPaths = async () => {
   const files = fs.readdirSync("posts")
   const paths = files.map(name => ({
-    params: { slug: slugify(name.replace(".mdx", "")) }
+    params: { slug: name.replace(".mdx", "") }
   }));
 
   return { paths, fallback: false }
