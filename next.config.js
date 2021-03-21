7e9bab152bc5256965cc306104c45ecef0735fa7
Kenneth Coffie <geniounico@outlook.com>
2021-03-21 20:08:15 +0000

    minor changes

--- next.config.js
+++ next.config.js
@@ -1,30 +1,30 @@
 module.exports = {
-  poweredByHeader: false,
-
   async redirects() {
     return [
       {
         source: 'v1',
         destination: "https://kennymark.github.io/",
+        permanent: true,
       },
       {
         source: '/v2',
         destination: 'https://kenny-v3.netlify.app/',
-
+        permanent: true,
       },
       {
         source: '/v3',
         destination: 'https://kenny-v3.netlify.app/',
-
+        permanent: true,
       },
       {
         source: '/stripe',
         destination: 'https://stripe-subscriptions.vercel.app/',
-
+        permanent: true,
       },
       {
         source: '/twitter',
         destination: 'https://twitter.com/mrkennymark',
+        permanent: true,
       },
     ]
   }
