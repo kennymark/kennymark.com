b70490d5b5ff1442e5d18b71455eabe5b55ff3a3
Kenneth Coffie <geniounico@outlook.com>
2021-03-22 18:43:43 +0000

    minor changes

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -16,7 +16,7 @@ export default function Profiles() {
 
         <chakra.div fontSize='xl' lineHeight="normal" color="gray.600">
           <chakra.p pb={5}>
-            I’m Kenny Coffie, currently a Software Engineer for SABS Travel Technologies. I am front-end focused developer with full stack experience. With a total of 5 years of experience, 3 years freelancing, studying and building hobby projects and 2 years commercial I have worked on every kind of application be it small, medium or very large enterprise application. I love everything UI and UX on the web but I have also worked on developed mobile applications, web apps and some backend applications.
+            I’m Kenny Coffie, currently a Software Engineer for Evergreen Health. I am front-end focused developer with full stack experience. With a total of 5 years of experience, 3 years freelancing, studying and building hobby projects and 2 years commercial I have worked on every kind of application be it small, medium or very large enterprise application. I love everything UI and UX on the web but I have also worked on developed mobile applications, web apps and some backend applications.
           </chakra.p>
 
           <chakra.p> I actively spend many hours per day working on my side projects which can be found on
