4342eae07c8378f2e4ec961f05398d07e868400a
Kenneth Coffie <geniounico@outlook.com>
2021-03-26 18:43:41 +0000

    updated readme and added functionality for copying code

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+Im Kenny, here is the code responsible for my portfolio/blog. Partly inspired by inspired by [`leerob website`](https://leerob.io) It's built with React, Next.js, useSwr etc.
+
+
+
 
 ## Getting Started
 
@@ -9,10 +12,19 @@ npm run dev
 # or
 yarn dev
 ```
-
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+
+## Deploy on Vercel
+One click deploy to make this your own.
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%kennymark%kennymark.com)
+
+
+## Overview
+
+- `pages/api/*` - [API routes](https://nextjs.org/docs/api-routes/introduction) powering [`/dashboard`](https://kennymark.com/dashboard), newsletter subscription, and post views.
+- `pages/blog/*` - Static pre-rendered blog pages using [MDX](https://github.com/mdx-js/mdx).
+- `pages/dashboard` - [Personal dashboard](https://kennymark.com/dashboard) containing metrics like sales, views, and subscribers.
 
 ## Learn More
 
@@ -23,8 +35,4 @@ To learn more about Next.js, take a look at the following resources:
 
 You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
 
-## Deploy on Vercel
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/import?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- src/components/mdx/code-block.tsx
+++ src/components/mdx/code-block.tsx
@@ -1,21 +1,35 @@
-import React, { useState } from 'react'
-import Highlight, { defaultProps } from 'prism-react-renderer'
-import lightTheme from 'prism-react-renderer/themes/dracula';
+import { Button, chakra, useClipboard, useColorModeValue } from '@chakra-ui/react';
+import Highlight, { defaultProps } from 'prism-react-renderer';
+import lightTheme from 'prism-react-renderer/themes/nightOwlLight';
 import darkTheme from 'prism-react-renderer/themes/oceanicNext';
-import { Box, Button, useClipboard, useColorModeValue } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
 
 export const CodeBlock = ({ children, className }) => {
   const language = className?.replace(/language-/, '')
   const theme = useColorModeValue(lightTheme, darkTheme)
-  const [value, setValue] = useState("Hello world")
+  const [value, setValue] = useState("")
   const { hasCopied, onCopy } = useClipboard(value)
+  const border = useColorModeValue('gray.100', 'gray.800')
 
+
+
+  useEffect(() => { setValue(children) }, [])
 
   return (
     <Highlight {...defaultProps} code={children} language={language} theme={theme}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <Box as='pre' borderRadius='lg' mb={5} p={25} overflowX='auto' rounded='lg' fontSize="md" pb={1} className={className}
-          style={{ ...style, }}>
+
+        <chakra.pre borderRadius='lg' mb={5} p={25} overflowX='auto' rounded='lg' pb={1} className={className}
+          style={{ ...style, }} border='1px' borderColor={border}>
+
+
+          <chakra.div pos='relative'>
+            <Button onClick={onCopy} ml={2} size='xs' bg='gray.300' pos='relative' float='right' top={-5} left={5} _focus={{ outline: 0 }}>
+              {hasCopied ? "Copied" : "Copy"}
+            </Button>
+          </chakra.div>
+
+
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({ line, key: i })}>
               {line?.map((token, key) => (
@@ -23,7 +37,7 @@ export const CodeBlock = ({ children, className }) => {
               ))}
             </div>
           ))}
-        </Box>
+        </chakra.pre>
       )}
     </Highlight>
   )
