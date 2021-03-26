54170b151c25db1a6eb87f2143e39cbc7b09c43f
Kenneth Coffie <geniounico@outlook.com>
2021-03-26 17:49:36 +0000

    minor changes

--- next.config.js
+++ next.config.js
@@ -52,6 +52,16 @@ module.exports = {
         source: '/_dev',
         destination: 'https://next-portfolio-git-dev-kennymmark.vercel.app/',
         permanent: true
+      },
+      {
+        source: '/test',
+        destination: 'https://next-portfolio-git-dev-kennymmark.vercel.app/',
+        permanent: true
+      },
+      {
+        source: '/v5',
+        destination: 'https://next-portfolio-git-dev-kennymmark.vercel.app/',
+        permanent: true
       }
     ]
   }
