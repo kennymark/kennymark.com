db485b93302612fbca7b7260698f1d0750f9b84e
“Kenny <kennymark@github.com>
2021-04-10 00:06:07 +0100

    fix

--- pages/slashes.tsx
+++ pages/slashes.tsx
@@ -1,12 +1,12 @@
 import { Container, Link, List, ListItem, Text } from '@chakra-ui/react'
 import SEO from '@components/seo'
 import React, { Fragment } from 'react'
-import { redirects } from '../next.config'
+import next from '../next.config'
 
 
 function Slashes() {
   let data;
-  async () => await redirects().then(links => data = links)
+  async () => await next.redirects().then(links => data = links)
   return (
     <Fragment>
       <SEO title="My slashes" />
