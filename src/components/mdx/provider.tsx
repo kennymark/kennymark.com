e4df00d91e442ecf9486943ebae5fc682c782229
“Kenny <kennymark@github.com>
2021-04-29 00:03:31 +0100

    fixed font

--- src/components/mdx/provider.tsx
+++ src/components/mdx/provider.tsx
@@ -1,60 +1,43 @@
-import { chakra, Code, Heading } from "@chakra-ui/react";
-import { MDXProvider } from "@mdx-js/react";
-import React from "react";
-import CodeBlock from "./code-block";
+import { chakra, Code, Heading, Text } from '@chakra-ui/react'
+import { MDXProvider } from '@mdx-js/react'
+import React from 'react'
+import CodeBlock from './code-block'
 
 export const components = {
-  h1: (props) => <Heading mb={10} textAlign="center" {...props} />,
-  h2: (props) => <Heading mt={5} mb={3} {...props} fontSize="4xl" />,
-  h3: (props) => <Heading mt={5} mb={3} {...props} fontSize="3xl" />,
+  h1: (props) => <Heading mb={10} textAlign='center' {...props} />,
+  h2: (props) => <Heading mt={5} mb={3} {...props} fontSize='4xl' />,
+  h3: (props) => <Heading mt={5} mb={3} {...props} fontSize='3xl' />,
   a: (props) => (
-    <chakra.a
-      target="_blank"
-      {...props}
-      color="blue.600"
-      _hover={{ color: "blue.800" }}
-    />
+    <chakra.a target='_blank' {...props} color='blue.600' _hover={{ color: 'blue.800' }} />
   ),
-  p: (props) => <chakra.p textStyle="p" {...props} />,
+  p: (props) => <Text fontSize='lg' py={1} {...props} />,
   ul: (props) => <chakra.ul p={5} {...props} />,
-  li: (props) => <chakra.li {...props} fontSize="lg" pb={1} color="gray.600" />,
-  pre: (props) => <chakra.pre borderRadius="lg" {...props} />,
+  li: (props) => <chakra.li {...props} fontSize='lg' pb={1} color='gray.600' />,
+  pre: (props) => <chakra.pre borderRadius='lg' {...props} />,
   img: (props) => (
-    <chakra.img
-      borderRadius="lg"
-      maxH={500}
-      borderColor="gray.200"
-      shadow="lg"
-      {...props}
-    />
+    <chakra.img borderRadius='lg' maxH={500} borderColor='gray.200' shadow='lg' {...props} />
   ),
-  code: (props) => <CodeBlock {...props} fontSize="md" />,
+  code: (props) => <CodeBlock {...props} fontSize='md' />,
   inlineCode: (props) => (
     <Code
-      as="span"
-      fontSize="md"
-      fontFamily="monospace"
-      rounded="lg"
+      fontSize='md'
+      fontFamily='monospace'
+      rounded='lg'
       letterSpacing={0.1}
       px={2}
       {...props}
-      colorScheme="orange"
+      colorScheme='orange'
     />
   ),
   blockquote: (props) => (
-    <chakra.blockquote
-      px={3}
-      borderLeft={4}
-      borderLeftColor="green.400"
-      {...props}
-    />
+    <chakra.blockquote px={3} borderLeft={4} borderLeftColor='green.400' {...props} />
   ),
-};
+}
 
 const MDXWrapper = (props) => (
   <MDXProvider components={components}>
     <article {...props} />
   </MDXProvider>
-);
+)
 
-export default MDXWrapper;
+export default MDXWrapper
