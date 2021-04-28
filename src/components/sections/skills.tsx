cbb9282d7b1dfd04ce23237addc20ba25e1f27f2
“Kenny <kennymark@github.com>
2021-04-29 00:09:22 +0100

    minor

--- src/components/sections/skills.tsx
+++ src/components/sections/skills.tsx
@@ -1,47 +1,76 @@
-
-import { Box, Heading, SimpleGrid, Text, useColorMode } from '@chakra-ui/react';
-import React from 'react';
-import { skillTypes } from '../../data/data';
+import { Box, Heading, SimpleGrid, Text, useColorMode } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
-
+import React from 'react'
+import { skillTypes } from '../../data/data'
 
 const MBox = motion(Box)
-
 
 function Skills() {
   const { colorMode } = useColorMode()
   const types = Object.keys(skillTypes)
-  const images = ['/images/front-end.svg', '/images/backend.svg', '/images/devops.svg', '/images/extras.svg']
-
+  const images = [
+    '/images/front-end.svg',
+    '/images/backend.svg',
+    '/images/devops.svg',
+    '/images/extras.svg',
+  ]
 
   return (
     <Box px={5} w={{ lg: 700, xl: 1400 }} mx='auto' mt={20}>
-      <Heading as='h1' mb={5} textAlign={['left', 'center']}>Skills</Heading>
+      <Heading as='h1' mb={5} textAlign={['left', 'center']}>
+        Skills
+      </Heading>
 
-      <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10} >
-        {types.map((type, key) =>
-          <MBox whileHover={{ scale: 1.04 }} transition={{ duration: 0.2 } as any}
-            borderRadius='lg' pb={8} key={type} position='relative'
-            bg={skillTypes[type].color + '.100'} zIndex={206}>
+      <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10}>
+        {types.map((type, key) => (
+          <MBox
+            whileHover={{ scale: 1.04 }}
+            transition={{ duration: 0.2 } as any}
+            borderRadius='lg'
+            pb={8}
+            key={type}
+            position='relative'
+            bg={skillTypes[type].color + '.100'}
+            zIndex={206}>
+            <Box
+              as='img'
+              src={images[key]}
+              alt={type}
+              pos='absolute'
+              width='inherit'
+              height='100%'
+              opacity={0.14}
+              zIndex={205}
+            />
 
-            <Box as='img' src={images[key]} alt={type} pos='absolute' width='inherit' height='100%' opacity={.14} zIndex={205} />
-
-            <Heading textAlign='center' as='h2' fontSize={20} textTransform='capitalize' my={5} color={colorMode && 'black'}>{type}
+            <Heading
+              textAlign='center'
+              as='h2'
+              fontSize={20}
+              textTransform='capitalize'
+              my={5}
+              color={colorMode && 'black'}>
+              {type}
             </Heading>
 
-            {skillTypes[type].skills.map(item => (
-              <Text py={2} textAlign='center' width='80%' borderRadius='lg' mb={2} mx='auto' color='gray.700' fontWeight='600' key={item}>
+            {skillTypes[type].skills.map((item) => (
+              <Text
+                py={2}
+                textAlign='center'
+                width='80%'
+                borderRadius='lg'
+                mb={2}
+                mx='auto'
+                color='gray.700'
+                fontWeight='600'
+                key={item}>
                 {item}
               </Text>
             ))}
-
           </MBox>
-        )}
-
+        ))}
       </SimpleGrid>
-
-    </Box >
-
+    </Box>
   )
 }
 
